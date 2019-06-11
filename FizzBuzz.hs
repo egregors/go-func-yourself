3bbbdaf1f07ff74de6a958d32f3a59b224e6c3d2
Vadim Iskuchekov <root@egregors.com>
2019-06-11 14:11:57 +0200

    typos

--- FizzBuzz.hs
+++ FizzBuzz.hs
@@ -1,7 +1,6 @@
 -- FizzBuss
-fizzbuzz :: Int -> String
 fizzbuzz n
     | n `mod` 15 == 0 = "FizzBuzz"
     | n `mod` 3 == 0 = "Fizz"
-    | n `mod` 5 == 0 = "Buss"
+    | n `mod` 5 == 0 = "Buzz"
     | otherwise = show n
